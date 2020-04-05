eb45d009ed813527d5fd9133fc45013a0a3947a4
Shuhey <shuhey.dev@gmail.com>
2020-04-05 16:08:31 +0900

    Add project files.

---
+++ EmbeddedFontsTest/EmbeddedFontsTest/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("fontello.ttf")]
+[assembly: ExportFont("ipam.ttf")]
+[assembly: ExportFont("CuteFont-Regular.ttf")]
+[assembly: ExportFont("Bellota-Regular.ttf",Alias ="Bellota")]
